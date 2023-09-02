6a8af27ee3940a861553f2a08a7784b46d0998ce
Srishti Kesarwani <kesarwanisrishti20@gmail.com>
2023-09-02 16:59:30 +0530

    render

--- frontend/src/services/apiSlice.js
+++ frontend/src/services/apiSlice.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:4002",
+  //baseUrl: "http://localhost:4002",
+  baseUrl: "https://nodejsvoosh.onrender.com",
 });
 
 const apiSlice = createApi({
